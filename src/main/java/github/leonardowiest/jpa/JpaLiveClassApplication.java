dedcfa68839218c94237c5f9bf71ced684fcea28
Leonardo Wiest <leonardowiest@hotmail.com>
2019-09-25 20:38:09 -0300

    Criado projeto Spring

---
+++ src/main/java/github/leonardowiest/jpa/JpaLiveClassApplication.java
@@ -0,0 +1,13 @@
+package github.leonardowiest.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaLiveClassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaLiveClassApplication.class, args);
+	}
+
+}
